0bdec35329669c87ee70e5968b6eb7e97af0a524
Renat <renat1.salimau@gmail.com>
2020-03-30 13:07:12 +0300

    feat(all project): pervaya versia saita sostouanie saita otlichno

---
+++ script.js
@@ -0,0 +1,28 @@
+function buttonclc() {
+    const btn = document.querySelector('button');
+    const theClipboard = navigator.clipboard;
+    console.log(theClipboard);
+
+    btn.addEventListener('click', () => {
+        let answer = confirm('Скопировать в буфер обмена???');
+        if (answer) alert('теперь вы стали обладателем рибки фугу')
+    })
+}
+
+
+
+function btnForImg() {
+    let count = 0;
+    const countPlace = document.getElementById('count-here');
+    document.getElementById('btn-for-img').addEventListener('click', () =>{
+        count++;
+        countPlace.innerText = count;
+
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    buttonclc();
+    btnForImg();
+});
+
